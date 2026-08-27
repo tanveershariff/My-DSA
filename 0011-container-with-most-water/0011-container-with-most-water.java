38f5d7965aaa9c225282734f3aad2e1febec1b12
Tanveer Shariff <sharifftanveer78@gmail.com>
2026-08-28 02:31:29 +0530

    Time: 5 ms (83.11%), Space: 77.3 MB (69.03%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -3,12 +3,11 @@ class Solution {
         int n = height.length;
         // Arrays.sort(height);
         int first = 0 , last = n-1;
-        // int result = 0, min = 0, wid = 0, 
-        int maxi = 0;
+        int result = 0, min = 0, wid = 0, maxi = 0;
         while(first!=last) {
-            int min = Math.min(height[first], height[last]);
-            int wid = last - first;
-            int result = min*wid;
+            min = Math.min(height[first], height[last]);
+            wid = last - first;
+            result = min*wid;
             maxi = Math.max(result, maxi);
             if(height[first]> height[last]){
                 last--;
